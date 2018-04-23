753927a369c5440071b96f497e4d15f4324d325b
Carlos Alatorre <carlos_alatorre_95@hotmail.com>
2018-04-23 13:02:44 -0700

    Se agregaron los archivos que faltaron en la seccion de zonas

---
+++ src/app/pages/zones/zones.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {BsModalService} from 'ngx-bootstrap';
+import {Globals} from '../../statics/globals';
+import {Zone} from '../../interfaces/zone';
+import {ZoneService} from '../../services/zone.service';
+import {ZoneComponent} from '../../modals/zone/zone.component';
+
+@Component({
+  selector: 'app-zones',
+  templateUrl: './zones.component.html',
+  styleUrls: ['./zones.component.scss']
+})
+export class ZonesComponent implements OnInit {
+
+    zones: Zone[];
+    zonesSearched: Zone[];
+    zoneFoundSearch: boolean = true;
+    existZones: boolean = false;
+    isLoading:boolean = false;
+
+    constructor(private _zoneService: ZoneService,
+                private _modalService: BsModalService) {
+    }
+
+    ngOnInit() {
+        this.isLoading = true;
+        this._zoneService.getZones().then((response: Zone[]) => {
+            this.isLoading = false;
+            this.zones = this.zonesSearched = response;
+            this.existZones = this.zones.length > 0;
+        });
+    }
+
+    zoneSearch(search: string) {
+        this.zonesSearched = this.zones.filter((it: Zone) => it.zonaNombre.toLowerCase().indexOf(search.toLowerCase()) >= 0);
+        this.zoneFoundSearch = this.zonesSearched.length > 0;
+    }
+
+    showZone(index: number) {
+        let modalRef = this._modalService.show(ZoneComponent, Object.assign({}, Globals.optionModalLg, {class: 'gray modal-lg'}));
+        modalRef.content.zone = this.zones[index];
+    }
+
+}
