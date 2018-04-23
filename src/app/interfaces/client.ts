ed613c8cef4121daab72fcd4f566725f9a438106
Carlos Alatorre <carlos_alatorre_95@hotmail.com>
2018-04-23 14:01:11 -0700

    Se crearon las funcionalidades de la seccion de servicios

--- src/app/interfaces/client.ts
+++ src/app/interfaces/client.ts
@@ -1,2 +1,9 @@
 export interface Client {
+    clienteDisponible:boolean
+    clienteDomicilio:string
+    clienteGuardias:any
+    clienteHorario:any
+    clienteNombre:string
+    clienteNumeroGuardias:number
+    clienteZonaAsignada:string
 }
--- src/app/modals/client-zone/client-zone.component.ts
+++ src/app/modals/client-zone/client-zone.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {GuardComponent} from '../guard/guard.component';
+import {GuardService} from '../../services/guard.service';
+import {Guard} from '../../interfaces/guard';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap';
+import {Globals} from '../../statics/globals';
+import {Client} from '../../interfaces/client';
+import {ClientZoneService} from '../../services/client-zone.service';
+import {Zone} from '../../interfaces/zone';
 
 @Component({
-  selector: 'app-client-zone',
-  templateUrl: './client-zone.component.html',
-  styleUrls: ['./client-zone.component.scss']
+    selector: 'app-client-zone',
+    templateUrl: './client-zone.component.html',
+    styleUrls: ['./client-zone.component.scss']
 })
 export class ClientZoneComponent implements OnInit {
 
-  constructor() { }
+    @Input() client: Client = {
+        clienteDisponible: null,
+        clienteDomicilio: null,
+        clienteGuardias: null,
+        clienteHorario: null,
+        clienteNombre: null,
+        clienteNumeroGuardias: null,
+        clienteZonaAsignada: null
+    };
 
-  ngOnInit() {
-  }
+    objectKeys = Object.keys;
+
+    constructor(private _activeModal: BsModalRef) {
+    }
+
+    ngOnInit() {
+
+    }
+
+    closeModal() {
+        this._activeModal.hide();
+    }
 
 }
